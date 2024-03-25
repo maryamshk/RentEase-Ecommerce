06e6f1678dc914faf9e2b8b924430df8c6f059cc
Maryam <maryamjaved.xo@gmail.com>
2024-03-25 21:05:46 +0500

    register user

--- frontend/src/screens/SignUp.js
+++ frontend/src/screens/SignUp.js
@@ -1,9 +1,102 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function SignUp() {
+  const [credentials, setCredentials] = useState({
+    name: '',
+    email: '',
+    password: '',
+    location: '',
+  });
+
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch('http://localhost:5001/auth/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ name: credentials.name, email: credentials.email, password: credentials.password, location: credentials.location })
+    });
+    const json = await response.json();
+    console.log(json);
+
+    if (!json.success) {
+      console.log("enter valid credentials")
+    }
+  }
+
+
   return (
-    <div></div>
-  )
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputUsername" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="exampleInputUsername"
+            onChange={onChange}
+            name="name"
+            value={credentials.name}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            name="email"
+            value={credentials.email}
+            onChange={onChange}
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            name="password"
+            value={credentials.password}
+            onChange={onChange}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Location
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            name="location"
+            value={credentials.location}
+            onChange={onChange}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default SignUp
+export default SignUp;
